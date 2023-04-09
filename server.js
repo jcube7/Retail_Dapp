77072aca196520aaa49dac94913b7bad2cbc569a
Jojo <jojotime7x@gmail.com>
2023-04-09 16:24:34 +0100

    port changes on server.js

--- server.js
+++ server.js
@@ -29,9 +29,7 @@ app.get("/account", (req, res) => {
 app.get("/user", (req, res) => {
   res.sendFile(path.join(__dirname + "/user.html"));
 });
-// serving the index.html file
 
-const server = app.listen(8000 || process.env.PORT());
+const server = app.listen(process.env.PORT || 8000);
 const portNumber = server.address().port;
 console.log(`port: ${portNumber}`);
-// can see the port number in terminal - you can dictate the port number
